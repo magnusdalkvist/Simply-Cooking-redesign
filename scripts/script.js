3266d91e44957851692582263d5923b5d40c5329
Magnus Dalkvist <magn552n@stud.kea.dk>
2021-12-10 02:03:27 +0100

    fixed nav and menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,10 @@
-document.querySelector(".red_button").addEventListener("click", changeRed);
-document.querySelector(".blue_button").addEventListener("click", changeBlue);
-document.querySelector(".green_button").addEventListener("click", changeGreen);
-document.querySelector(".orange_button").addEventListener("click", changeOrange);
-document.querySelector(".standard_button").addEventListener("click", changeStandard);
+document.querySelector(".closeMenu").addEventListener("click", closeMenu)
+document.querySelector(".openMenu").addEventListener("click", openMenu)
 
-function changeRed() {
-    document.querySelector("body").className = "red"
+function openMenu() {
+    document.querySelector("#menu").style.height = "100%";
 }
 
-function changeBlue() {
-    document.querySelector("body").className = "blue"
-}
-
-function changeGreen() {
-    document.querySelector("body").className = "green"
-}
-
-function changeOrange() {
-    document.querySelector("body").className = "orange"
-}
-
-function changeStandard() {
-    document.querySelector("body").className = "standard"
+function closeMenu() {
+    document.querySelector("#menu").style.height = "0%";
 }
