d22c0bc1b04c6a51c9f20644ba9f322c028fe7b4
Magnus Dalkvist <magn552n@stud.kea.dk>
2021-12-10 13:00:08 +0100

    nav final

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
 window.addEventListener("scroll", navColor)
+window.addEventListener("load", navColor)
 
 function navColor() {
     if (window.scrollY > 0) {
-        document.querySelector("#mobile").classList.remove("nocolor");
         document.querySelector("#web").classList.remove("nocolor");
     } else {
-        document.querySelector("#mobile").classList.add("nocolor");
         document.querySelector("#web").classList.add("nocolor");
     }
 }
