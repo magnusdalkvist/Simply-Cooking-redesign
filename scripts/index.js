805b90b07ef0faf9012af26298ab837d444cb6e8
Magnus Dalkvist <magn552n@stud.kea.dk>
2021-12-13 09:09:03 +0100

    updated nav

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ window.addEventListener("load", navColor)
 
 function navColor() {
     if (window.scrollY > 0) {
-        document.querySelector("#web").classList.remove("nocolor");
+        document.querySelector(".web").classList.remove("nocolor");
     } else {
-        document.querySelector("#web").classList.add("nocolor");
+        document.querySelector(".web").classList.add("nocolor");
     }
 }
