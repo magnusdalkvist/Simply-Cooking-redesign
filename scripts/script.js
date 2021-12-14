efb8570106f65ec2ad06e8081f69023aa8f51ea5
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2021-12-14 07:41:47 +0100

    vælg-uge-menu virker ikke

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,14 @@ function openMenu() {
 function closeMenu() {
   document.querySelector("#menu").style.height = "0%";
 }
+
+// funktioner til 'Vælg Uge'-knap under Ugens Menu
+// let uger = document.querySelector(".uger");
+// uger.classList.add(".hide_uge");
+// console.log("gemuger");
+
+document.querySelector("#jan").addEventListener("toggle", openJan);
+function openJan() {
+  console.log("click jan");
+  document.querySelector("jan_uger").classList.remove(".hide");
+}
